869680c2dc5aac3eabce4e57e3924a7eb12a7202
036_Charudatta_Shimpi_CM_AC <charudattashimpi19230@gmail.com>
2026-05-01 21:59:38 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,16 @@
+// Express Js : Library to develop the backend  (3rd party module or dependency) 
+
+//get the url on which req has arrived
+
+// import { response } from "express"
+
+// const currentUrl = request.url()
+
+// if(currentUrl === "www.facebook.com"){
+
+// }else if (currentUrl === "www.facebook.com/home"){
+
+// }
+
+
+//routing : what response should be there for a particular url
